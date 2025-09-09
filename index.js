e2f86ec7896680483e1e4acdaa896464b86d8017
e1q23079 <e1q23079@oit.ac.jp>
2025-09-10 01:38:11 +0900

    データベースアクセス

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello, World!");
+import postgres from "postgres";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL;
+
+const sql = postgres(connectionString);
+
+const main = async () => {
+
+    console.log("Accessing the database...");
+
+    const data = await sql`
+    SELECT *
+    FROM blog
+    WHERE id = 1
+    LIMIT 1;
+`
+
+    console.log(data[0]['title'], data[0]['text']);
+
+    console.log("Access complete.");
+
+    process.exit(0);
+
+}
+
+main();
